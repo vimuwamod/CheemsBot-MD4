540006f42d69142eed47e8f00ea97efa66beb554
vimuwamod <102043707+vimuwamod@users.noreply.github.com>
2022-07-11 11:54:56 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -38,7 +38,7 @@ global.APIKeys = {
 //aumto functioner
 global.autoTyping = false //auto tying in gc (true to on, false to off)
 global.autoreadpmngc = false //auto reading in gc and pm (true to on, false to off)
-global.autoReadGc = false //auto reading in gc (true to on, false to off)
+global.autoReadGc = true //auto reading in gc (true to on, false to off)
 global.autoRecord = false //auto recording (true to on, false to off)
 global.available = true //auto available (true to on, false to off)
 
@@ -54,19 +54,19 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
+global.vcardowner = ['94761062584'] //ur owner number
+global.ownername = "༻𝙼.𝚁.ᏣᎽᏰᏋ ᏉᎥᎷ𝐔Ꮙ𝐀༺" //ur owner name
+global.ytname = "YT: Vimuwa" //ur yt chanel name
+global.socialm = "GitHub: Vimuwa" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
+global.owner = ['94761062584']
+global.ownertag = '94761062584' //ur tag number
 global.botname = 'Cheems Bot MD V4' //ur bot name
-global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
+global.linkz = "https://youtube.com/channel/UCmof3JiLcV9uwVrYuCaqOKw" //your theme url which will be displayed on whatsapp
+global.websitex = "https://youtube.com/channel/UCmof3JiLcV9uwVrYuCaqOKw" //ur website to be displayed
+global.botscript = 'https://github.com/vimuwamod/' //script link
 global.reactmoji = "😘" //ur menu react emoji
 global.themeemoji = "🦄" //ur theme emoji
 global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
